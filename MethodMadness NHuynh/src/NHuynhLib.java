b268c1326366b87b0b165aa2f27eeeffd222779d
nickn <nickn@192.168.1.2>
2017-10-11 19:37:37 -0400

    finished isPalindrome

---
+++ MethodMadness NHuynh/src/NHuynhLib.java
@@ -0,0 +1,33 @@
+
+public class NHuynhLib {
+	public static void isPalindrome(String a)
+	{
+		
+		int left = 0;
+		int right = a.length() - 1;
+			while (left < right)
+				if (a.charAt(left ++) == a.charAt(right--))
+					return;
+					
+	}	
+	public static boolean isFibonacci(int a)
+	{
+		int n1 = 0;
+		int n2 = 1;
+		int n3 = 1;
+			while (n3 < a)
+			{
+				n3 = n2 + n1;
+				n1 = n2;
+				n2 = n3;
+			}
+				if (n3 == a )
+					return true;
+				else 
+					return false;
+	}
+	public static void sumUpTo()
+	{
+		
+	}
+}
--- MethodMadness NHuynh/src/Runner.java
+++ MethodMadness NHuynh/src/Runner.java
@@ -1,10 +1,10 @@
+
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ControlStructure.isPalindrome();
-		ControlStructure.isFibonacci(1);
-		ControlStructure.sumUpTo();
+		
+		NHuynhLib.isFibonacci(1);
+		NHuynhLib.sumUpTo();
 		
 	}
 
